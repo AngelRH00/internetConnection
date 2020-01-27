2127fae670c28d73e5681767a41542d362b6f534
angel <raising125@gmail.com>
2020-01-27 20:57:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/internetconnection/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.internetconnection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mensajeria;
+    private EditText pagina;
+    private String conexion;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mensajeria = findViewById(R.id.cajaMensaje);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void sacarTextoWeb(View view) {
+        mensajeria = findViewById(R.id.cajaMensaje);
+        pagina = findViewById(R.id.pagina);
+        conexion = pagina.getText().toString();
+        new web().execute();
+
+    }
+    private class web extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String mensaje = "";
+
+            try {
+                URL url = new URL(conexion);
+
+                // read text returned by server
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                String line = "";
+
+                //requiredString = s.substring(s.indexOf("(") + 1, s.indexOf(")"));
+                mensaje = "";
+                while ((line = in.readLine()) != null) {
+                mensaje+=line;
+                }
+                in.close();
+
+            } catch (MalformedURLException e) {
+                System.out.println("Malformed URL: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("I/O Error: " + e.getMessage());
+            }
+            String regexString = Pattern.quote("<p>") + "(?s)(.*?)" + Pattern.quote("</p>");
+            Pattern pattern = Pattern.compile(regexString);
+// mensaje contains the full text that you want to extract data
+            Matcher matcher = pattern.matcher(mensaje);
+
+            String finalMessage = "";
+            while (matcher.find()) {
+                 finalMessage += matcher.group(1); // Since (.*?) is capturing group 1
+                // You can insert match into a List/Collection here
+            }
+            finalMessage = finalMessage.substring(0,finalMessage.indexOf("<"));
+            String finalmensaje2 = finalMessage.replace(".", ".\n");
+            finalMessage = finalMessage.replace("?", "?\n");
+            return finalMessage;
+        }
+        protected void onPostExecute(String finalMessage){
+            mensajeria.setText(finalMessage);
+        }
+    }
+
+
+}
+
